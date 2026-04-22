593166161c8292ccbcb93d3483a30cfe8728fb6f
Alain Poirier <alain.poirier@net-ng.com>
2026-04-22 10:59:03 +0200

    Assets reloading moved down to `nagare.server.http_application`

--- src/nagare/server/mvc_application.py
+++ src/nagare/server/mvc_application.py
@@ -10,53 +10,14 @@
 from nagare.renderers import html5_base
 from nagare.server.http_application import Request, RESTApp, Response  # noqa: F401
 
-
-def livereload(event, dirname, filename, reloader, url):
-    if filename.endswith(('.css', '.js', '.gif', '.png', '.jpeg', '.jpg')):
-        reloader.reload_asset(url + '/' + filename)
-
-    return None
-
-
 # ---------------------------------------------------------------------------
 
 
 class App(RESTApp):
     """Application to handle a HTTP request."""
 
-    CONFIG_SPEC = RESTApp.CONFIG_SPEC | {
-        'default_content_type': 'string(default="text/html")',
-        'static_url': 'string(default="/static$app_url")',
-        'static': 'string(default="$_static_path")',
-        'gzip_static': 'boolean(default=True)',
-    }
+    CONFIG_SPEC = RESTApp.CONFIG_SPEC | {'default_content_type': 'string(default="text/html")'}
     renderer_factory = html5_base.Renderer
-
-    def __init__(self, name, dist, static_url, static, gzip_static, services_service, **config):
-        services_service(
-            super().__init__,
-            name,
-            dist,
-            static_url=static_url,
-            static=static,
-            gzip_static=gzip_static,
-            **config,
-        )
-
-        self.static_url = static_url.rstrip('/')
-        self.static_path = static.rstrip('/')
-        self.gzip_static = gzip_static
-
-    def handle_start(self, app, services_service, statics_service, reloader_service=None):
-        services_service(super().handle_start, app)
-
-        if self.static_url:
-            statics_service.register_dir(self.static_url, self.static_path, self.gzip_static)
-
-        if (reloader_service is not None) and (self.static_path):
-            reloader_service.watch_dir(
-                self.static_path, livereload, recursive=True, reloader=reloader_service, url=self.static_url
-            )
 
     def create_renderer(self, **params):
         """Create the initial renderer."""
